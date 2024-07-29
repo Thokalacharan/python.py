07e6c1d9a17f2d304e96482c41d9a9fff9645b5e
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-07-29 16:54:13 +0530

    Create speed of vehicle.py

---
+++ speed of vehicle.py
@@ -0,0 +1,11 @@
+
+# write a program to find the speed of a vehicle
+d=float(input("enter the distance in kms"))
+t=float(input("enter the time min"))
+s=d/t
+print(s,"kmph")
+
+output:
+enter the distance in kms60
+enter the time min60
+1.0 kmph
