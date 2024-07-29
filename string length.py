724567b5c38eff926c97f5f9e8638d0999a147cd
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-07-29 17:54:54 +0530

    Create string length.py

---
+++ string length.py
@@ -0,0 +1,18 @@
+
+# write a program to print the each character of a string and string length
+str=input("enter the string")
+i=0
+while i<len(str):
+  print(str[i])
+  i=i+1
+print(len(str))
+
+
+enter the stringcharan
+c
+h
+a
+r
+a
+n
+6
