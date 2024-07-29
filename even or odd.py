bfd458cc1c6df8479eba83f4ad96d4aa9761102c
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-07-29 16:56:11 +0530

    Create even or odd.py

---
+++ even or odd.py
@@ -0,0 +1,11 @@
+
+# check the given number is even or odd
+a=float(input("enter a"))
+if a==2:
+    print("given number is even")
+else:
+    print("the number is  odd")
+
+output:
+enter a5
+the number is  odd
