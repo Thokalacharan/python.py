ccc0d912def0fcdd1e12303e469468375ee5f58e
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-07-29 17:50:13 +0530

    Create sum of 10 natural numbers.py

---
+++ sum of 10 natural numbers.py
@@ -0,0 +1,11 @@
+# program to find the sum of first 10 natural numbers(while loop)
+n=int(input("enter the value"))
+sum=0
+i=1
+while i<=n:
+  sum=sum+i
+  i=i+1
+print(sum)
+
+enter the value10
+55
