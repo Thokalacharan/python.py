79d99164e9f99162249601dcd5894ad34fbbb98d
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-07-29 17:49:11 +0530

    Create reverse star.py

---
+++ reverse star.py
@@ -0,0 +1,18 @@
+
+ # print the *
+n=int(input("enter the n"))
+i=5
+while i>=1:
+  j=1
+  while j<=i:  
+    print('*',end='')
+    j=j+1
+  print()
+  i=i-1   
+
+enter the n5
+*****
+****
+***
+**
+*
