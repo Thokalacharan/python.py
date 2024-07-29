649806ee35eb65682c936888db61523249c8f6c8
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-07-29 17:54:15 +0530

    Create for loop table.py

---
+++ for loop table.py
@@ -0,0 +1,17 @@
+
+# write a program multiplication table (for loop)
+n=int(input("enter the value"))
+for i in range(1,11):
+  print(n,"*",i,"=",n*i)
+
+enter the value5
+5 * 1 = 5
+5 * 2 = 10
+5 * 3 = 15
+5 * 4 = 20
+5 * 5 = 25
+5 * 6 = 30
+5 * 7 = 35
+5 * 8 = 40
+5 * 9 = 45
+5 * 10 = 50
