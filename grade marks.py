a9143ee4599572df1c5f7fdcd780004b21828887
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-07-29 17:45:49 +0530

    Create grade marks.py

---
+++ grade marks.py
@@ -0,0 +1,19 @@
+
+# write a program to determine the grade based on marks
+a=float(input("enter the marks"))
+if a>=90:
+      print("grade a")
+elif a>=80 and a<=90:
+      print("grade b")
+elif a>=70 and a<=80:
+      print("grade c")
+elif a>=60 and a<=70:
+      print("grade a")
+elif a>=50 and a<=60:
+      print("grade a")
+else:
+      print("failed")
+
+output:
+enter the marks2
+failed
