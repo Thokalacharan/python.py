c0fc8539a8597536525081ba77c0164107ed5671
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-07-29 17:52:35 +0530

    Create table.py

---
+++ table.py
@@ -0,0 +1,19 @@
+
+# write a program multiplication table (while loop)
+n=int(input("enter the value"))
+i=1
+while i<=10:
+  print(n,"*",i,"=",n*i)
+  i=i+1
+
+enter the value5
+5 * 1 = 5
+5 * 2 = 10
+5 * 3 = 15
+5 * 4 = 20
+5 * 5 = 25
+5 * 6 = 30
+5 * 7 = 35
+5 * 8 = 40
+5 * 9 = 45
+5 * 10 = 50
