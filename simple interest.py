4a09f9f671a19b3bf4feb0dd190db46c1d535c0a
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-07-29 16:51:47 +0530

    Create simple interest.py

---
+++ simple interest.py
@@ -0,0 +1,13 @@
+
+# write a program to find simple interest
+p=float(input("enter the P value",))
+t=float(input("enter the T value"))
+r=float(input("enter the R value"))
+SI=(p*t*r)/100
+print(SI)
+
+output:
+enter the P value123
+enter the T value20
+enter the R value10
+246.0
