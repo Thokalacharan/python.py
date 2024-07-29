72d7f0317e4af4183b2d5394af06656f0f95900e
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-07-29 16:50:26 +0530

    Create fahrenheit to celsius.py

---
+++ fahrenheit to celsius.py
@@ -0,0 +1,7 @@
+# write a program to convert fahrenheit to celsius
+f=float(input("enter the fahrenheit"))
+c=(f-32)*5/9
+print(" the celsius",c)
+output:
+enter the fahrenheit98
+ the celsius 36.666666666666664
