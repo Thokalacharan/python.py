ddb5e71560eaefe69c767a3288b06bc4711b2dba
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-07-29 17:48:37 +0530

    Create star.py

---
+++ star.py
@@ -0,0 +1,18 @@
+
+# print the *
+n=int(input("enter the n"))
+i=1
+while i<=n:
+  j=1
+  while j<=i:  
+    print('*',end='')
+    j=j+1
+  print()
+  i=i+1   
+
+enter the n5
+*
+**
+***
+****
+*****
