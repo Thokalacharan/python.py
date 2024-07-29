f49bffc164bbb721eaca1e6021d0da9dad9bfbe3
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-07-29 17:46:51 +0530

    Create day of week.py

---
+++ day of week.py
@@ -0,0 +1,25 @@
+
+# prompt: determine the day of a week
+
+day = float(input("Enter a number between 1 and 7: "))
+
+if day == 1:
+  print("Sunday")
+elif day == 2:
+  print("Monday")
+elif day == 3:
+  print("Tuesday")
+elif day == 4:
+  print("Wednesday")
+elif day == 5:
+  print("Thursday")
+elif day == 6:
+  print("Friday")
+elif day == 7:
+  print("Saturday")
+else:
+  print("Invalid input. Please enter a number between 1 and 7.")
+
+
+Enter a number between 1 and 7: 1
+Sunday
