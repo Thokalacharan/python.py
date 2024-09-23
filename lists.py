c8dc8a221eae0c353301e012885fa413939317aa
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-09-23 15:46:13 +0530

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,67 @@
+#  find sum and average
+l= [1,2,3]
+print("sum=",sum(l))
+print("average=",sum(l)/len(l))
+
+
+# reverse of list
+l=[1,2,3]
+l.reverse()
+print(l)
+
+
+# reverse the list using slicing
+l= [1,2,3,4,45]
+print(l[ ::-1])
+
+
+# write a program to extract even and odd number from a given list
+l =[]
+n= int(input("enter the size of list"))
+for i in range(n):
+    ele= int(input())
+    l.append(ele)
+even=[]
+odd=[]
+for i in l:
+    if i%2==0:
+        even.append(i)
+    else:
+        odd.append(i)
+print("even=",even)
+print("odd=",odd)
+
+
+# write a program to find the position of minimum and maximum element in the list
+l=[]
+n= int(input("enter the size of list"))
+for i in range(n):
+    ele= int(input("enter the elements"))
+    l.append(ele)
+print(l)
+print("max=",max(l))
+print("min=",min(l))
+
+
+# interchange the first and last element in the list
+l =[1,2,34,5,6]
+temp=l[0]
+l[0]=l[-1]
+l[-1]=temp
+print(l)
+
+
+# write a program to remove first occurrence of a given element in the list
+l= [1,2,3,1,5,1,2,]
+l.remove(1)
+print(l)
+
+
+# concatenate two lists
+list1 = ["M", "na", "i", "cha"]
+list2 = ["y", "me", "s", "ran"]
+list3=[list1[0]+list2[0]]
+list3.append(list1[1]+list2[1])
+list3.append(list1[2]+list2[2])
+list3.append(list1[3]+list2[3])
+print(list3)
