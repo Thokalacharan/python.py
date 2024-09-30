a667ee45d4f5d718fb4a7abba7a5793f47233017
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-09-30 15:32:50 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,39 @@
+# strings
+
+str1= 'hello'
+str2= "charan"
+str3= '''naidu'''
+print(str1,str2,str3)
+
+# string indexing
+str1= 'hello'
+print(str1[0])
+print(str1[1])
+print(str1[2])
+print(str1[3])
+print(str1[4])
+
+
+# strings operations
+# concatenation
+str1= 'hello'
+str2= "charan"
+print(str1+str2)
+
+# repetition
+str1= 'hello'
+print(str1*3)
+
+# membership
+str1= 'hello'
+print('h' in str1)
+print('a' not in str1)
+
+# iterating through a string
+str1= 'hello'
+for i in str1:
+    print(i)
+
+# slicing
+str1= 'hello'
+print(str1[1:4])
