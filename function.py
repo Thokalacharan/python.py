ffde3246d9563429640ca4bda54a46a7a2d65150
Thokala Charan <166838214+Thokalacharan@users.noreply.github.com>
2024-10-21 16:21:29 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,23 @@
+
+
+def f(a,b,c):
+  print(a,b,c)
+f(2,3,4)    
+
+
+#write functions for basic arithmetic operations(addtion,sub,mul,division).create a program that two numbers as input and returns the result
+def add(a,b):
+  return a+b
+def sub(a,b):
+  return a-b
+def mul(a,b):
+  return a*b
+def div(a,b):
+  return a/b
+sub(3,5)  
+
+
+# write a program function with multiple return values
+def add_sub_div(a,b):
+  return a+b,a-b
+add_sub(2,3)
